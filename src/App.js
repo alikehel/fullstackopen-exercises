646d1395e5e0f8890335306d4b32d60f38834365
Ali Kehel <ali.saber.27.2@gmail.com>
2022-07-24 16:04:10 +0200

    finish '1.2: course information, step2'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{props.title}</h1>
+  )
 }
 
-export default App;
+const Content = (props) => {
+  return (
+    // props.parts.foreach((part) => {
+    //   return `<p>{part}<p/>`
+    // })
+    // <>
+    //   <p>{props.parts[0]} {props.exercises[0]}</p>
+    //   <p>{props.parts[1]} {props.exercises[1]}</p>
+    //   <p>{props.parts[2]} {props.exercises[2]}</p>
+    // </>
+    <div>
+      <Part name={props.parts[0]} count={props.exercises[0]} />
+      <Part name={props.parts[1]} count={props.exercises[1]} />
+      <Part name={props.parts[2]} count={props.exercises[2]} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.count}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.count}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+      <Total count={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
