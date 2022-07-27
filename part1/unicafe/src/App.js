f4b9f0e409d85bf2435e534c0f2a481d97e76492
Ali Kehel <ali.saber.27.2@gmail.com>
2022-07-27 05:21:46 +0200

    finish '1.7: unicafe step2'

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,14 +4,44 @@ const Header = ({ name }) => <h1>{name}</h1>
 
 const Button = ({ name, onClick }) => <button onClick={onClick}>{name}</button>
 
+// const Statistic = (props) => {
+
+// }
+
 const Statistics = ({ data }) => {
   // console.log(data)
+  let newData = data.map((element, index) => {
+    return (
+      <p key={index} >{element[0]}: {element[1]}</p>
+    )
+  })
+
+  // ALL
+  let all = 0
+  data.forEach(element => {
+    all += element[1]
+  });
+
+  // AVERAGE
+  let allWeights = 0, average
+  data.forEach((element) => {
+    allWeights += element[1] * element[2]
+    // console.log(element[2], allWeights)
+  });
+  average = allWeights / all || 0
+
+  // POSITIVE
+  let positive = (data[0][1] / all) * 100 || 0
+
+
+
   return (
-    data.map(element => {
-      return (
-        <p>data: {element}</p>
-      )
-    })
+    <>
+      {newData}
+      <p>All: {all}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive} %</p>
+    </>
   )
 }
 
@@ -27,7 +57,7 @@ const App = () => {
       <Button name='Natural' onClick={() => setNatural(natural + 1)} />
       <Button name='Bad' onClick={() => setBad(bad + 1)} />
       <Header name='Statistics' />
-      <Statistics data={[good, natural, bad]} />
+      <Statistics data={[['good', good, 1], ['natural', natural, 0], ['bad', bad, -1]]} />
     </div>
   )
 }
