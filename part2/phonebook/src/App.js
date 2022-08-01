3ba8addb80e9db854dbcc6584e6bf5218ca5a1f0
Ali Kehel <ali.saber.27.2@gmail.com>
2022-08-01 10:52:16 +0200

    finish '2.6: The Phonebook Step1'

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,43 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
-  const [newName, setNewName] = useState('')
+    const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+    const [newName, setNewName] = useState('');
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
-}
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const newPerson = {
+            name: newName
+        }
+        setPersons([...persons, newPerson])
+        setNewName('')
+    };
 
-export default App
+    const handleChange = (e) => {
+        setNewName(e.target.value);
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    name: <input value={newName} onChange={handleChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>
+                {
+                    persons.map((person) => {
+                        return <p key={person.name}>{person.name}</p>;
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+export default App;
