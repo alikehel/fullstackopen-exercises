3ef05a4f3616776f003aa2eab9816f682a327e76
Ali Kehel <ali.saber.27.2@gmail.com>
2022-08-04 18:19:23 +0200

    '2.12* Data for countries, step1' not complete

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,8 +1,47 @@
-function App() {
-  return (
-    <div>
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
-    </div>
+function App() {
+  const [search, setSearch] = useState("")
+  const [countries, setCountries] = useState([])
+  const [searchedCountries, setSearchedCountries] = useState([])
+
+  const handleChange = (e) => {
+    const searchValue = e.target.value
+    setSearch(searchValue)
+    console.log(searchValue, search)
+    const filteredCountries = countries.filter((country) => {
+      return country.name.common.toLowerCase().includes(search.toLowerCase())
+    })
+    setSearchedCountries(filteredCountries)
+    console.log(filteredCountries)
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('https://restcountries.com/v3.1/all')
+      const data = await response.data
+      // console.log(data[0])
+      setCountries(data)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      <div>
+        <label>Find Countries: </label>
+        <input value={search} onChange={handleChange}></input>
+      </div>
+      <div>
+        {
+          searchedCountries &&
+          searchedCountries.map((country, index) => {
+            return <p key={index}>{country.name.common}</p>
+          })
+        }
+      </div>
+    </>
   );
 }
 
