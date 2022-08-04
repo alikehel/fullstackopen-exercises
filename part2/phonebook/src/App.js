6c1a8a02478d80e7874ff392e96a13077de2f3a6
Ali Kehel <ali.saber.27.2@gmail.com>
2022-08-04 15:00:08 +0200

    finish '2.11: The Phonebook Step6'

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Search from "./components/Search";
 import AddNumber from "./components/AddNumber";
 import Numbers from "./components/Numbers";
+import axios from 'axios'
 
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+        // { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ]);
     const [searchedPersons, setSearchedPersons] = useState([]);
     const [searching, setSearching] = useState(false);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:3003/persons').then(response => {
+            const persons = response.data
+            setPersons(persons)
+        })
+    }, [])
 
     const handleNameChange = (e) => {
         setNewName(e.target.value);
