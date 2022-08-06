8f0d782c2fb36e3f9097da1e54c0aaeb6c06508e
Ali Kehel <ali.saber.27.2@gmail.com>
2022-08-06 14:10:37 +0200

    add flags for '2.12* Data for countries, step1'

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -44,22 +44,34 @@ function App() {
       <div>
         {
           (searchedCountries.length > 10)
-            ? <p>More Than 10</p>
-            : (searchedCountries.length == 1)
+            ?
+            (
+              <p>More Than 10</p>
+            )
+            :
+            (searchedCountries.length == 1)
               ?
-              <div>
-                <h1>{searchedCountries[0].name.common}</h1>
-                <p>Capital: {searchedCountries[0].capital}</p>
-                <h2>languages</h2>
-                <ul>
-                  {Object.values(searchedCountries[0].languages).map((lang, index2) => {
-                    return <li key={index2}>{lang}</li>
-                  })}
-                </ul>
-              </div>
-              : searchedCountries.map((country, index) => {
-                return <p key={index}>{country.name.common}</p>
-              })
+              (
+                <div>
+                  <h1>{searchedCountries[0].name.common}</h1>
+                  <p>Capital: {searchedCountries[0].capital}</p>
+                  <h2>languages</h2>
+                  <ul>
+                    {
+                      Object.values(searchedCountries[0].languages).map((lang, index2) => {
+                        return <li key={index2}>{lang}</li>
+                      })
+                    }
+                  </ul>
+                  <img src={searchedCountries[0].flags.png} />
+                </div>
+              )
+              :
+              (
+                searchedCountries.map((country, index) => {
+                  return <p key={index}>{country.name.common}</p>
+                })
+              )
         }
       </div>
     </>
